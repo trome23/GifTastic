0bec4173aecef0dabcc0a18122cee26aa9c936f9
trome23 <romennoodles23@yahoo.com>
2019-03-18 18:46:09 -0600

    displays API results in console

---
+++ assets/Javascript/GifTastic.js
@@ -0,0 +1,10 @@
+//  VARIABLES
+//  --------------------------------------------
+    //API Key to GIPHY
+var authKey = "1bNGrlJ2oTvuncnETFmqLXNtUA7R8kr2";
+
+//  FUNCTIONS
+//  --------------------------------------------
+
+    //using ajax to access the south park GIPHY API
+    var queryURL: "https://api.giphy.com/v1/gifs/search?api_key=1bNGrlJ2oTvuncnETFmqLXNtUA7R8kr2&q=south%20park&limit=10&offset=0&rating=G&lang=en"; 
